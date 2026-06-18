54def34dffd3809d51ed7a707df80fa1ce6afeca
murkavetta-ship-it <murkavetta@gmail.com>
2026-06-18 23:28:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHANNEL_ID = -1002373540662          # Ваш основной канал "Брендменю"
 CHANNEL_ID_SISTER = -1001857424835   # Канал "Шоппинг" сестры
 ARCHIVE_CHANNEL_ID = -1004331909805  # ВАШ РЕАЛЬНЫЙ АРХИВ ДЛЯ ТАЙМЕРОВ ФОТО!
-MORNING_CHANNEL_ID = -1003783532522  # ВАШ КАНАЛ С КАРТИНКАМИ УТРА!
+MORNING_CHANNEL_ID = -1003735848662  # ВАШ КАНАЛ С КАРТИНКАМИ УТРА!
 
 SETTINGS_FILE = "settings_v2.json"
 
